9868e6a936e3e48ec40121e48472b33e8873fe89
Jancker Sepúlveda <sepulved.janck@gmail.com>
2023-03-30 12:48:38 -0400

    [ADD]:seed permissions, roles & user + post M&m

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +17,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $postsCreate = Permission::create(['name' => 'post.create']);
+        $postsUpdate = Permission::create(['name' => 'post.update']);
+
+        $admin = Role::create([
+            'name' => 'ADMIN'
+        ]);
+
+        $publishier = Role::create([
+            'name' => 'PUBLICADOR'
+        ]);
+
+        $visitor = Role::create([
+            'name' => 'VISITANTE'
+        ]);
+
+        //Asignning permissions to role
+        $publishier->givePermissionTo($postsCreate);
+        $publishier->givePermissionTo($postsUpdate);
+
+        User::create([
+            'name' => 'Jancker',
+            'email' => 'sepulved.janck@gmail.com',
+            'password' => Hash::make('12345678'),
+        ])->assignRole(['ADMIN', 'PUBLICADOR']);
+        //App\Models\User::assignRole
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +22,38 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+//views
+Route::get('/posts', 'App\Http\Controllers\PostController@index');
+Route::get('/posts/create', 'App\Http\Controllers\PostController@createview');
+Route::get('/posts/update', 'App\Http\Controllers\PostController@updateview');
+//actions
+Route::post('/posts', 'App\Http\Controllers\PostController@store');
+Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update');
+Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@delete');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//views
+Route::get('/users', 'App\Http\Controllers\UserController@index');
+Route::get('/users/create', 'App\Http\Controllers\UserController@createview');
+Route::get('/users/update', 'App\Http\Controllers\UserController@updateview');
+//actions
+Route::post('/users', 'App\Http\Controllers\UserController@store');
+Route::put('/users/{id}', 'App\Http\Controllers\UserController@update');
+Route::delete('/users/{id}', 'App\Http\Controllers\UserController@delete');
 
-Auth::routes();
+//views
+Route::get('/roles', 'App\Http\Controllers\RoleController@index');
+Route::get('/roles/create', 'App\Http\Controllers\RoleController@createview');
+Route::get('/roles/update', 'App\Http\Controllers\RoleController@updateview');
+//actions
+Route::post('/roles', 'App\Http\Controllers\RoleController@store');
+Route::put('/roles/{id}', 'App\Http\Controllers\RoleController@update');
+Route::delete('/roles/{id}', 'App\Http\Controllers\RoleController@delete');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//views
+Route::get('/permissions', 'App\Http\Controllers\PermissionController@index');
+Route::get('/permissions/create', 'App\Http\Controllers\PermissionController@createview');
+Route::get('/permissions/update', 'App\Http\Controllers\PermissionController@updateview');
+//actions
+Route::post('/permissions', 'App\Http\Controllers\PermissionController@store');
+Route::put('/permissions/{id}', 'App\Http\Controllers\PermissionController@update');
+Route::delete('/permissions/{id}', 'App\Http\Controllers\PermissionController@delete');
